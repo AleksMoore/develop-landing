65ca9f6b8ef66f41b719a8cabdaa9c7f95519f0a
Alex Moore <34859574+AleksMoore@users.noreply.github.com>
2019-04-24 11:15:50 +0300

    Add files via upload

---
+++ js/all.js
@@ -0,0 +1,38 @@
+/* Slider */
+
+$(function() {
+ $('.slider__wrapper').owlCarousel({
+ 	items: 5,
+    margin: 0,
+    loop: true,
+    nav: true
+ });
+});
+
+/* End Slider */
+
+/* Company */
+
+$(function() {
+ $('.company__wrapper').owlCarousel({
+ 	items: 1,
+    margin: 0,
+    loop: true,
+    nav: true
+ });
+});
+
+/* End Company */
+
+/* Reviews */
+
+$(function() {
+ $('.reviews__wrapper').owlCarousel({
+ 	items: 1,
+    margin: 0,
+    loop: true,
+    nav: true
+ });
+});
+
+/* End Reviews */
